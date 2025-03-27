11d67be7a3d58dcb762c6d167189f5a75ec1f3eb
Diogo Dourado <douradofadiogo@gmail.com>
2025-03-27 13:50:35 -0300

    [refactor][feat] Implement simple API integrated with locust load testing

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,20 +1,26 @@
 from locust import HttpUser, task, between
-from api.services.post_service import PostService
-from data.test_data import TestData
 
-class ApiUser(HttpUser):
-    # Specify the base host here
-    host = "https://jsonplaceholder.typicode.com"
 
-    wait_time = between(1, 5)
-
-    def on_start(self):
-        self.post_service = PostService(self.client)
+class UserApiUser(HttpUser):
+    wait_time = between(1, 3)  # Wait between 1-3 seconds between tasks
 
     @task
-    def get_posts(self):
-        self.post_service.get_posts()
+    def create_user(self):
+        self.client.post("/users", json={
+            "name": "Locust User",
+            "email": f"user{self.user_id}@locust.com"
+        })
 
-    @task(3)
-    def create_post(self):
-        self.post_service.create_post(TestData.POST_DATA)
+    @task(3)  # 3x more likely to run than create
+    def get_users(self):
+        self.client.get("/users")
+
+    @task(2)
+    def get_single_user(self):
+        # First create a user to ensure one exists
+        create_resp = self.client.post("/users", json={
+            "name": "Test User",
+            "email": "test@locust.com"
+        })
+        user_id = create_resp.json()["id"]
+        self.client.get(f"/users/{user_id}")
